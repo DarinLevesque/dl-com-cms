14d25c6a870a5b9320b0147bc230e0223cb89792
Darin Levesque <darin.levesque@gmail.com>
2019-01-31 22:10:33 -0500

    Testing TypeIt finctionality

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -4,6 +4,14 @@ import PropTypes from "prop-types";
 import Article from "../Main/Article";
 import PageHeader from "./PageHeader";
 import Content from "../Main/Content";
+import TypeIt from 'typeit';
+
+const instance = new TypeIt('#replaceStrings', {
+  strings: ["This is a great string.", "But here is a better one."],
+  speed: 50,
+  breakLines: false,
+  waitUntilVisible: true
+}).go();
 
 const Page = props => {
   const { page } = props;
@@ -12,6 +20,7 @@ const Page = props => {
   return (
     <Article>
       <PageHeader {...page.frontmatter} />
+      <p id="replaceStrings"></p>
       <Content html={html} />
     </Article>
   );
