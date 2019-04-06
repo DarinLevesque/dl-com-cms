eebd8b792952ef1798f3dea55887e409cccdaa59
Darin Levesque <darinlevesque@gmail.com>
2019-04-05 22:40:41 -0400

    remove spotify integration...for now

--- gatsby-config.js
+++ gatsby-config.js
@@ -62,17 +62,17 @@ module.exports = {
         chunkSize: 10000 // default: 1000
       }
     },
-    {
-      resolve: `gatsby-source-spotify`,
-      options: {
-        clientId: process.env.SPOTIFY_CLIENT_ID,
-        clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-        refreshToken: process.env.SPOTIFY_REFRESH_TOKEN,
-        fetchPlaylists: true, // optional. Set to false to disable fetching of your playlists
-        fetchRecent: true, // optional. Set to false to disable fetching of your recently played tracks
-        timeRanges: ['short_term', 'medium_term', 'long_term'], // optional. Set time ranges to be fetched
-      },
-    },
+    // {
+    //   resolve: `gatsby-source-spotify`,
+    //   options: {
+    //     clientId: process.env.SPOTIFY_CLIENT_ID,
+    //     clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+    //     refreshToken: process.env.SPOTIFY_REFRESH_TOKEN,
+    //     fetchPlaylists: true, // optional. Set to false to disable fetching of your playlists
+    //     fetchRecent: true, // optional. Set to false to disable fetching of your recently played tracks
+    //     timeRanges: ['short_term', 'medium_term', 'long_term'], // optional. Set time ranges to be fetched
+    //   },
+    // },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
